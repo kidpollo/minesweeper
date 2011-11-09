fdb51085f2d333017b54fe7d8ab81a4221a11da6
@kidpollo <kidpollo@gmail.com>
2011-11-08 17:02:58 -0800

    Adds create and show methods

--- app/controllers/minesweeper_controller.rb
+++ app/controllers/minesweeper_controller.rb
@@ -3,4 +3,13 @@ class MinesweeperController < ApplicationController
     
   end
 
+  def create
+    @board = Board.new(params[:board])
+    @board.save
+    redirect_to "/#{@board.name.gsub(" ","_")}"
+  end
+  
+  def show
+    
+  end
 end
