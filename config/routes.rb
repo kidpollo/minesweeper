752dea13f1d3de1d202df93b3868b934c38e3fb5
@kidpollo <kidpollo@gmail.com>
2011-11-08 17:04:11 -0800

    Adds post and show routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 MineSweeper::Application.routes.draw do
-  get "minesweeper/welcome"
+  get  "minesweeper/welcome"
+  post "minesweeper/create"
+  
+  match '/:name' => 'minesweeper#show'
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
@@ -47,7 +50,7 @@ MineSweeper::Application.routes.draw do
   #     # (app/controllers/admin/products_controller.rb)
   #     resources :products
   #   end
-
+  
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
    root :to => 'minesweeper#welcome'
